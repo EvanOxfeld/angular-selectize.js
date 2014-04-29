562ad8c07d1e74e256f6c58ad619d4a085f72aa4
Evan Oxfeld <eoxfeld@gmail.com>
2014-04-29 17:15:38 -0400

    Wrap in immediately-invoked function expression

--- angular-selectize.js
+++ angular-selectize.js
@@ -1,5 +1,3 @@
-'use strict';
-
 /**
  * Directive to convert a select into a selectize.js hybrid textbox and <select>
  * Supports an ngOptions expression. Tested with:
@@ -14,89 +12,96 @@
  *   ng-options="option.id as option.name for option in options"
  *   selectize="{ plugins: ['remove_button'], create: 'true' }">
  * </select>
+ *
+ * Attributes:
+ *   multiple: Converts the select into text input of tags
+ *
+ * (c) 2014 Evan Oxfeld https://github.com/EvanOxfeld/angular-selectize.js
+ * License: MIT
+**/
 
-     Attributes:
-    multiple: Converts the select into text input of tags
- **/
+(function (angular) {
+  'use strict';
 
-angular.module('selectize', [])
+  angular.module('selectize', [])
 
-.directive('selectize', ['$parse', function($parse) {
-  var NG_OPTIONS_REGEXP = /^\s*([\s\S]+?)(?:\s+as\s+([\s\S]+?))?(?:\s+group\s+by\s+([\s\S]+?))?\s+for\s+(?:([\$\w][\$\w]*)|(?:\(\s*([\$\w][\$\w]*)\s*,\s*([\$\w][\$\w]*)\s*\)))\s+in\s+([\s\S]+?)(?:\s+track\s+by\s+([\s\S]+?))?$/;
+  .directive('selectize', ['$parse', function($parse) {
+    var NG_OPTIONS_REGEXP = /^\s*([\s\S]+?)(?:\s+as\s+([\s\S]+?))?(?:\s+group\s+by\s+([\s\S]+?))?\s+for\s+(?:([\$\w][\$\w]*)|(?:\(\s*([\$\w][\$\w]*)\s*,\s*([\$\w][\$\w]*)\s*\)))\s+in\s+([\s\S]+?)(?:\s+track\s+by\s+([\s\S]+?))?$/;
 
-  return {
-    scope: {
-      multiple: '@',
-      opts: '@selectize'
-    },
-    require: '?ngModel',
-    link: function(scope, element, attrs, ngModelCtrl) {
-      var opts = scope.$parent.$eval(scope.opts) || {};
-      var match = attrs.ngOptions.match(NG_OPTIONS_REGEXP);
-      var valueName = match[4] || match[6];
-      var optionsProperty = match[7];
-      var valueFn = $parse(match[2] ? match[1] : valueName);
-      var selectize;
+    return {
+      scope: {
+        multiple: '@',
+        opts: '@selectize'
+      },
+      require: '?ngModel',
+      link: function(scope, element, attrs, ngModelCtrl) {
+        var opts = scope.$parent.$eval(scope.opts) || {};
+        var match = attrs.ngOptions.match(NG_OPTIONS_REGEXP);
+        var valueName = match[4] || match[6];
+        var optionsProperty = match[7];
+        var valueFn = $parse(match[2] ? match[1] : valueName);
+        var selectize;
 
-      scope.$parent.$watch(function() {
-        return ngModelCtrl.$modelValue;
-      }, function(newValues, oldValues) {
-        if (selectize && !angular.equals(newValues, oldValues) && !modelMatchesView()) {
-          refreshSelectize();
-        }
-      });
-
-      function modelMatchesView() {
-        var model = ngModelCtrl.$modelValue;
-        model = model && !angular.isArray(model) ? [model] : model || [];
-        var view = selectize.items.map(function(item) {
-          return getOptionValue(scope.$parent[optionsProperty][item]);
+        scope.$parent.$watch(function() {
+          return ngModelCtrl.$modelValue;
+        }, function(newValues, oldValues) {
+          if (selectize && !angular.equals(newValues, oldValues) && !modelMatchesView()) {
+            refreshSelectize();
+          }
         });
-        return angular.equals(model, view);
-      }
 
-      scope.$parent.$watchCollection(optionsProperty, refreshSelectize);
-
-      function refreshSelectize(newOptions, oldOptions) {
-        if (selectize) {
-          selectize.destroy();
+        function modelMatchesView() {
+          var model = ngModelCtrl.$modelValue;
+          model = model && !angular.isArray(model) ? [model] : model || [];
+          var view = selectize.items.map(function(item) {
+            return getOptionValue(scope.$parent[optionsProperty][item]);
+          });
+          return angular.equals(model, view);
         }
-        scope.$evalAsync(function() {
-          $(element).selectize(opts);
-          selectize = $(element)[0].selectize;
-          if (scope.multiple) {
-            selectize.on('item_add', function(value, $item) {
-              var model = ngModelCtrl.$viewValue;
-              var option = scope.$parent[optionsProperty][value];
-              value = option ? getOptionValue(option) : value;
 
-              model.push(value);
-              scope.$evalAsync(function() {
-                ngModelCtrl.$setViewValue(model);
-              });
-            });
-            selectize.on('item_remove', function(value) {
-              var model = ngModelCtrl.$viewValue;
-              var option = scope.$parent[optionsProperty][value];
-              value = option ? getOptionValue(option) : value;
+        scope.$parent.$watchCollection(optionsProperty, refreshSelectize);
 
-              var index = model.indexOf(value);
-              if (index >= 0) {
-                model.splice(index, 1);
+        function refreshSelectize(newOptions, oldOptions) {
+          if (selectize) {
+            selectize.destroy();
+          }
+          scope.$evalAsync(function() {
+            $(element).selectize(opts);
+            selectize = $(element)[0].selectize;
+            if (scope.multiple) {
+              selectize.on('item_add', function(value, $item) {
+                var model = ngModelCtrl.$viewValue;
+                var option = scope.$parent[optionsProperty][value];
+                value = option ? getOptionValue(option) : value;
+
+                model.push(value);
                 scope.$evalAsync(function() {
                   ngModelCtrl.$setViewValue(model);
                 });
-              }
-            });
-          }
-        });
-      }
+              });
+              selectize.on('item_remove', function(value) {
+                var model = ngModelCtrl.$viewValue;
+                var option = scope.$parent[optionsProperty][value];
+                value = option ? getOptionValue(option) : value;
 
-      function getOptionValue(option) {
-        var optionContext = {};
-        optionContext[valueName] = option;
-        return valueFn(optionContext);
+                var index = model.indexOf(value);
+                if (index >= 0) {
+                  model.splice(index, 1);
+                  scope.$evalAsync(function() {
+                    ngModelCtrl.$setViewValue(model);
+                  });
+                }
+              });
+            }
+          });
+        }
+
+        function getOptionValue(option) {
+          var optionContext = {};
+          optionContext[valueName] = option;
+          return valueFn(optionContext);
+        }
       }
-    }
-  };
-}]);
+    };
+  }]);
+})(angular);
